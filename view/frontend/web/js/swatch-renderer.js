48feda7a5d3d8a0edb8409794df9569c75988812
ElHarfali Abdelkebir <elharfali.abdelkebir@gmail.com>
2020-12-24 16:48:57 +0000

    first commit

---
+++ view/frontend/web/js/swatch-renderer.js
@@ -0,0 +1,110 @@
+define([
+    'jquery'
+], function ($) {
+    'use strict';
+
+    return function (widget) {
+        console.log('Hello from SwatchExtend');
+
+        $.widget('mage.SwatchRenderer', widget, {
+            _Rebuild: function () {
+                console.log('Hello from rebuild method ----------------------');
+                return this._super();
+            }
+        });
+
+        return $.mage.SwatchRenderer;
+    }
+});
+
+
+
+
+
+define([
+    'jquery',
+    'jquery/ui',
+    'Magento_Swatches/js/swatch-renderer'
+], function($){
+
+    $.widget('godogi.SwatchRenderer', $.mage.SwatchRenderer, {
+      /**
+       * Return intersection from 2 dimentional array
+       *
+       * @returns {Array}
+       * @private
+       */
+
+      _IntersectionArray: function () {
+        var result = [];
+        var lists;
+
+        if(arguments.length === 1) {
+          lists = arguments[0];
+        } else {
+          lists = arguments;
+        }
+
+        for(var i = 0; i < lists.length; i++) {
+          var currentList = lists[i];
+          for(var y = 0; y < currentList.length; y++) {
+            var currentValue = currentList[y];
+            if(result.indexOf(currentValue) === -1) {
+              var existsInAll = true;
+              for(var x = 0; x < lists.length; x++) {
+                if(lists[x].indexOf(currentValue) === -1) {
+                  existsInAll = false;
+                  break;
+                }
+              }
+              if(existsInAll) {
+                result.push(currentValue);
+              }
+            }
+          }
+        }
+        return result;
+      },
+      /**
+       * Rebuild container
+       *
+       * @private
+       */
+      _Rebuild: function () {
+          var $widget = this,
+              controls = $widget.element.find('.' + $widget.options.classes.attributeClass + '[attribute-id]'),
+              selected = controls.filter('[option-selected]');
+          var selectedProducts = [],
+              intersectionProducts = [];
+
+          // Enable all options
+          $widget._Rewind(controls);
+
+          // done if nothing selected
+          if (selected.length <= 0) {
+              return;
+          }
+          selected.each(function () {
+            var $this = $(this),
+                id = $this.attr('attribute-id'),
+                products = $widget._CalcProducts(id);
+
+            selectedProducts.push($widget.optionsMap[id][$this.attr('option-selected')].products);
+
+          });
+          console.log('selectedProducts:');
+          console.log(selectedProducts);
+
+          intersectionProducts = $widget._IntersectionArray(selectedProducts);
+          console.log('intersection:');
+          console.log(intersectionProducts);
+          if(intersectionProducts.length < 1 || intersectionProducts == undefined){
+            document.getElementById("product-addtocart-button").disabled = true;
+          } else {
+            document.getElementById("product-addtocart-button").disabled = false;
+          }
+      }
+    });
+
+    return $.godogi.SwatchRenderer;
+});
